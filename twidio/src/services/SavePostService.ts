111f7928c4d25621d4ac74006f173acd222c1ab1
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-27 14:25:51 -0300

    refactor: save post service and its test

--- twidio/src/services/SavePostService.ts
+++ twidio/src/services/SavePostService.ts
@@ -1,16 +1,28 @@
+/* eslint-disable space-before-function-paren */
 import { getCustomRepository } from 'typeorm'
 import { PostRepository } from './../repositories/PostRepository'
 import { Post } from '../entities/Post'
 
+interface ISavePostService {
+  postRepository?: PostRepository
+  author: string
+  content: string
+
+}
 export class SavePostService {
   private postRepository: PostRepository
+  private post: Post
 
-  // eslint-disable-next-line space-before-function-paren
-  constructor(postRepository: PostRepository = getCustomRepository(PostRepository)) {
+  constructor({
+    postRepository = getCustomRepository(PostRepository),
+    author,
+    content
+  }: ISavePostService) {
     this.postRepository = postRepository
+    this.post = new Post(author, content)
   }
 
-  async execute(post: Post): Promise<Post> {
-    return await this.postRepository.save(post)
+  async execute(): Promise<Post> {
+    return await this.postRepository.save(this.post)
   }
 }
