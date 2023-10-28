dba95f16a13fd790deedad5b8f8b4902e8d91ca4
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-28 05:40:58 -0300

    chore: add save post test to integration tests

--- twidio/tests/posts.test.ts
+++ twidio/tests/posts.test.ts
@@ -1,11 +1,13 @@
 import axios from 'axios'
+import createConnection from '../src/database'
 
 const server = axios.create({
-  baseURL: 'http://localhost:5001'
+  baseURL: 'http://localhost:5001/v1'
 })
 
 describe('/posts', () => {
-  it('Should return status 200 and list of players', async () => {
+  jest.setTimeout(5000)
+  it('Should return status 200 and list of posts', async () => {
     const expectedResponse = [
       {
         post_id: 'b0f3d72a-b8a9-45ec-a8f0-a14f65f3597d',
@@ -22,5 +24,22 @@ describe('/posts', () => {
     const response = await server.get('/posts')
     expect(response.status).toBe(200)
     expect(response.data).toMatchObject(expectedResponse)
+  })
+
+  it('Should return status 200 when post is saved', async () => {
+    const response = await server.post('/post', {
+      author: 'some@email.dio',
+      content: 'Some tuit about that'
+    })
+
+    const connection = await createConnection()
+    await connection.query(`DELETE FROM posts WHERE post_id = ${response.data.post_id}`)
+    await connection.close()
+
+    expect(response.status).toBe(201)
+    expect(response.data).toMatchObject({
+      author: 'some@email.dio',
+      content: 'Some tuit about that'
+    })
   })
 })
